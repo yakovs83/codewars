0a029a9545ee2b30034596b1624a5483fefd3efe
Sergey <sergeyyak@gmail.com>
2015-11-22 17:50:38 -0800

    More fixes. Still does not pick up the sell side

--- src/StockBroker.hs
+++ src/StockBroker.hs
@@ -2,7 +2,8 @@ module StockBroker where
 import Text.Read
 import Data.Maybe
 import Data.List
-input = "CLH15.NYM 50 56.32 B, ZNGA 1300 2.66 B, OWW 1000 11.623 B, OGG 20 580.1 B"
+input1 = "CLH15.NYM 50 56.32 B, ZNGA 1300 2.66 B, OWW 1000 11.623 B, OGG 20 580.1 B"
+input2 = "GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"
 
 data Result = Correct {quote::String, 
                        quantity::Int, 
@@ -15,9 +16,7 @@ parseQuote str = let lengthOk = (length . words $ str) == 4
                     else let [quo,qtt,pr,st] = words str
                              qttInt = readMaybe qtt::Maybe Int
                              prDouble = readMaybe pr::Maybe Double
-                             prOk = case pr of
-                                 h:'.':t1:t -> True
-                                 _ -> False
+                             prOk = elem '.' pr --not quite bulletproof, can miss cases like "50." 
                              stOk = st=="B" || st=="S"
                          in if (isJust qttInt && isJust prDouble && prOk && stOk)
                             then Correct quo (fromJust qttInt) (fromJust prDouble) st
@@ -28,10 +27,15 @@ splitOrder "" = []
 splitOrder str = let (h,t) = break (==',') str
                  in h:splitOrder (drop 1 t)
 
+
+foldFun::(Double,Double)->Result->(Double,Double)
+foldFun acc (Correct _ qt pr st)
+    | st == "B" = (fst acc + (fromIntegral qt)*pr,snd acc)
+    | st == "S" = (fst acc, snd acc + (fromIntegral qt)*pr)
+    | otherwise = acc --not ideal, probably should throw here or somehow handle this case
+
 addCorrect::[Result]->String
-addCorrect cs = let (buy,sell) = foldl (\acc (Correct _ qt pr st) -> if (st == "B") 
-                                                                        then (fst acc + (fromIntegral qt)*pr,snd acc) 
-                                                                        else (fst acc, snd acc + (fromIntegral qt)*pr)) (0.0,0.0) cs
+addCorrect cs = let (buy,sell) = foldl foldFun (0.0,0.0) cs
                 in "Buy: " ++ (show . round $ buy) ++ " Sell: " ++ (show . round $ sell)
 
 grpFun::Result->Result->Bool
@@ -46,5 +50,5 @@ balanceStatement st = let ord = groupBy grpFun $ map parseQuote (splitOrder st)
                           
 
 main = do
-    putStrLn $ balanceStatement input
-    --putStrLn $ head . drop 1 . splitOrder $ input
+    putStrLn $ balanceStatement input1
+    putStrLn $ balanceStatement input2
